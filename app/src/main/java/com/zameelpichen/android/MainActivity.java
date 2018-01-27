54ab2ccca6b082059d86993ef515795277d14796
zameel <zameelpichen@gmail.com>
2018-01-27 08:29:24 +0530

    completed import

---
+++ app/src/main/java/com/zameelpichen/android/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zameelpichen.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.zameelpichen.android.liveVideoPlayer.LiveVideoPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String RTMP_BASE_URL = "rtmp://192.168.43.168/vod/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.zameelpichen.android.liveVideoBroadcaster.R.layout.activity_main);
+    }
+
+    public void openVideoBroadcaster(View view) {
+        Intent i = new Intent(this, io.antmedia.android.liveVideoBroadcaster.LiveVideoBroadcasterActivity.class);
+        startActivity(i);
+    }
+
+    public void openVideoPlayer(View view) {
+        Intent i = new Intent(this, LiveVideoPlayerActivity.class);
+        startActivity(i);
+    }
+}
